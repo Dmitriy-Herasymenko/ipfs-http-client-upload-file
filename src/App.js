e63cca26e99a186e8bf034936ffbd0fda9f6580a
embellisher <shiningxors@gmail.com>
2021-04-13 15:43:24 +0300

    create components upload-ipfs class method and functional

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+'use strict'
+const React = require('react')
+import IPFSLOADCLASS from './components/Upload';
+import IPFLOADFN from './components/UploadFn';
+
+function App() {
+  
+    return (
+      <div className="App">
+          <IPFLOADFN />
+      </div>
+    );
+  }
+module.exports = App
---
+++ src/components/UploadFn.js
@@ -0,0 +1,59 @@
+const { useState, useEffect } = require('react')
+
+const React = require('react')
+const IPFS = require('ipfs-core')
+
+const UploadFn = () => {
+    const [hash, setHash] = useState("");
+    const [fileName, setFileName] = useState("");
+    const [pathFileName, setPathFileName] = useState([]);
+  
+    useEffect(() => {
+
+    const saveToIpfsWithFilename = async ([ file ]) => {
+      const fileDetails = {
+        path: file.name,
+        content: file
+      }
+      const options = {
+        wrapWithDirectory: true,
+        progress: (prog) => console.log(`received: ${prog}`)
+      }
+      const ipfs = await IPFS.create()
+      const { cid } = await ipfs.add(fileDetails, options)
+      const result = {hash: cid.string, fileName: fileDetails.path}
+      return result;
+    }
+    
+    const captureFile = async files => {
+      const data = await saveToIpfsWithFilename(files);
+      setHash(data.hash);
+      setFileName(data.fileName);
+    }
+    
+    if (pathFileName.length) {
+        captureFile(pathFileName);
+      }
+
+  },[pathFileName]);
+  
+    const handelChangeFile = e => {
+      setPathFileName(e.target.files);
+    }
+   
+    return (
+      <div className="App">
+        <form>
+             <input type='file' name='input-file' onChange={handelChangeFile}/><br/>
+           </form>
+           <div>
+            <a target='_blank'
+              href={'https://ipfs.io/ipfs/'+hash+"/"+fileName}> {hash}
+            </a>
+          </div>       
+      </div>
+    );
+}
+
+export default UploadFn;
+  
